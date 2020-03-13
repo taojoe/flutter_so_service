d3eccf6d41f9216da056087d3da4c979e3c8e3ea
joe <taojoe@gmail.com>
2020-03-13 23:28:31 +0800

    init project

---
+++ ios/Classes/SoServicePlugin.m
@@ -0,0 +1,20 @@
+#import "SoServicePlugin.h"
+
+@implementation SoServicePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"so_service"
+            binaryMessenger:[registrar messenger]];
+  SoServicePlugin* instance = [[SoServicePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
